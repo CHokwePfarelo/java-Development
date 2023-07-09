853c2a346e47395a2b67eb0ffb6729bb0ad8d6e8
Pfarelo Chokwe <4022839@myuwc.ac.za>
2023-07-09 10:31:27 +0200

    <Solution to the firts problem>

---
+++ taskOne.java
@@ -0,0 +1,29 @@
+import.java utill Scanner;
+
+class taskOne{
+
+ int search(int elem[]){
+   
+  for(i=0,i<elem.length,i++){
+    
+     if(elem[i] == input){ 
+     return;}
+
+  }
+ }
+
+
+public static void main(String args[]){
+       int match;
+ int [] array = {2,3,5,7};
+
+   taskOne obj = new taskOne();
+
+  Scanner objct = new Scanner(system.in);
+   System.out.println("Enter a number");
+    int input = objct.nextInt();
+
+   match = obj.search(array);
+         System.out.println("A match has been found to be" + match);
+         }
+         

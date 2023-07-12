04183387191ce7eea9d48c2db4f72329e801f399
Pfarelo Chokwe <4022839@myuwc.ac.za>
2023-07-13 00:33:36 +0200

    Calculator

---
+++ calcula.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+
+
+public class calcula {
+
+
+
+   ///////Method
+   float value = 0;
+   float compu(float num1; opper ; float num2){
+      
+    switch (opper) {
+        case '+':
+        float aa = num1+num2;
+        break;
+        case '/':
+        float aa = num1/num2;
+        break;
+        case '-':
+        float aa = num1-num2;
+        break;
+        }
+
+
+
+    return aa;
+   }
+    
+
+    
+
+
+public static void main(String arguments[]){
+
+
+     Scanner scan = new Scanner(System.in);
+
+
+  System.out.print("Enter a number");
+
+   float num1 = scan.nextInt();
+
+ System.out.print("Enter a opperation :");
+
+    opper = scab.
+
+ System.out.print("Enter a number :");
+
+
+   float num2 = scan.nextInt();
+
+
+
+     calcula obj = new calcula(num1 opper num2);
+
+     float ans = obj.compu();
+     System.out.println("Ans =" + ans);
+
+
+
+}
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
